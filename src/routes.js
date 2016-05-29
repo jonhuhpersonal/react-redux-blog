b21fb7b4a64769ed904f8c290827339562180984
Jon Huh <jonhuhpersonal@gmail.com>
2016-05-29 01:21:47 -0700

    [progress] add route.js file

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/app';
+
+//mapping of url to components
+
+export default (
+	<Route path="/" component={App} />
+);
