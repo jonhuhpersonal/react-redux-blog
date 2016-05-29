98a69a6d60feae848a040fd8422dec740298fd98
Jon Huh <jonhuhpersonal@gmail.com>
2016-05-29 01:38:55 -0700

    [progress] adds index route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import PostsIndex from './components/posts_index'
 
 //mapping of url to components
 
@@ -13,6 +14,7 @@ export default (
 
 	//App won't know where to render Greeting unless you specify.
 	<Route path="/" component={App}>
+		<IndexRoute component={PostsIndex} />
 		<Route path="greet" component={Greeting} />
 		<Route path="greet2" component={Greeting} />
 		<Route path="greet3" component={Greeting} />
