94bc6ab1e71960c452a68ff77ceb7fe1cff94ce4
Jon Huh <jonhuhpersonal@gmail.com>
2016-05-29 01:41:52 -0700

    [add] installs axios and redux-promise modules

--- src/routes.js
+++ src/routes.js
@@ -6,22 +6,10 @@ import PostsIndex from './components/posts_index'
 
 //mapping of url to components
 
-const Greeting = () => {
-	return <div>Hey there!</div>;
-}
-
 export default (
 
 	//App won't know where to render Greeting unless you specify.
 	<Route path="/" component={App}>
 		<IndexRoute component={PostsIndex} />
-		<Route path="greet" component={Greeting} />
-		<Route path="greet2" component={Greeting} />
-		<Route path="greet3" component={Greeting} />
 	</Route>
 );
-
-//   /           App
-//   /greet      App, Greeting
-//   /greet2     App, Greeting
-//   /greet3     App, Greeting
