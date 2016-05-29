e64af5b24b1f98d332068116a107e3ccb109c437
Jon Huh <jonhuhpersonal@gmail.com>
2016-05-29 01:23:48 -0700

    [bugfix] fix typo in index.js file's imports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { Rounter, browserHistory } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 
 import reducers from './reducers';
 import routes from './routes';
